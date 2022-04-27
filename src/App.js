52e9ab15e89fdff288c5a983a3ab227c9c392fd8
Norman Teodoro <normanteodoro@hotmail.com>
2022-04-27 09:52:22 -0700

    fixed height

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/layouts/Layout'
 import './App.css'
-import Main from './components/pages/Main'
 import Header from './components/layouts/Header'
 import Projects from './components/pages/Projects'
 import Socials from './components/pages/Socials'
 import Resume from './components/pages/Resume'
+import Home from './components/pages/Home'
 
 export default function App () {
   const [ selectedComponent, setSelectedComponent ] = useState('0')
@@ -16,7 +16,7 @@ export default function App () {
       <Header selectedComponent={selectedComponent} setSelectedComponent={setSelectedComponent}/>
         <Layout>          
           <Routes>
-            <Route path='/' element={<Main />} />
+            <Route path='/' element={<Home />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/socials' element={<Socials />} />
             <Route path='/resume' element={<Resume />} />
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,20 +1,8 @@
 import Home from './Home'
-import Projects from './Projects'
-import Resume from './Resume'
-import Socials from './Socials'
-
-export default function Main () {    
-    
+export default function Main () {        
   return (
     <>
-    <Home />
-      <div className='expandable' id='about'>
-        {' '}
-        This is the about me page{' '}
-      </div>
-        <Projects />      
-        <Socials />
-        <Resume />
+    <Home />    
     </>
   )
 }
