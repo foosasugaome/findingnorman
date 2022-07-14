8289513747189a6dc0a875fca3cfc701f14d8e04
Norman Teodoro <normanteodoro@hotmail.com>
2022-07-13 19:00:31 -0700

    fixed padding on page resize

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -19,58 +19,64 @@ export default function Contact () {
       .then(response => response.json())
       .then(data => {
         setServerMessage(data)
-        setForm({name: '',
-        email: '',
-        message: ''})
+        setForm({ name: '', email: '', message: '' })
       })
       .catch(error => {
         console.error('Error:', error)
       })
   }
   return (
-    <>  
-      <motion.div className='forms' initial={{ opacity: 0 }}
+    <>
+      <motion.div
+        className='forms'
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ type: 'spring', delay: 0, duration: 1 }}>
+        transition={{ type: 'spring', delay: 0, duration: 1 }}
+      >
         <div className='hello'>
-        <h1><strong>Say hello!</strong></h1>
+          <h1>
+            <strong>Say hello!</strong>
+          </h1>
         </div>
-      <motion.div initial={{ x: '100vw'}} animate={{ x:20 }} transition={{ type: 'spring', delay: 0, duration: 0.8}} className='form-container'>
-      <form onSubmit={submitForm}>
-        <label htmlFor='name'></label>
-        <input
-          type='text'
-          id='name'
-          value={form.name}
-          onChange={e => setForm({ ...form, name: e.target.value })}
-          placeholder='name'
-          required
-        />
-        <label htmlFor='email'></label>
-        <input
-          type='email'
-          id='email'
-          value={form.email}
-          onChange={e => setForm({ ...form, email: e.target.value })}
-          placeholder='email'
-          required
-        />
-        <label htmlFor='message'></label>
-        <textarea
-          id='message'
-          value={form.message}
-          onChange={e => setForm({ ...form, message: e.target.value })}
-          placeholder='message'
-        />
-        <button type='submit'>Send</button>
-      </form>
-      <p>
-        {JSON.stringify(serverMessage) ? serverMessage.msg : null}
-      </p>
-      </motion.div>
+        <motion.div
+          initial={{ x: '100vw' }}
+          animate={{ x: 20 }}
+          transition={{ type: 'spring', delay: 0, duration: 0.8 }}
+          className='form-container'
+        >
+          <form onSubmit={submitForm}>
+            <label htmlFor='name'></label>
+            <input
+              type='text'
+              id='name'
+              value={form.name}
+              onChange={e => setForm({ ...form, name: e.target.value })}
+              placeholder='name'
+              required
+            />
+            <label htmlFor='email'></label>
+            <input
+              type='email'
+              id='email'
+              value={form.email}
+              onChange={e => setForm({ ...form, email: e.target.value })}
+              placeholder='email'
+              required
+            />
+            <label htmlFor='message'></label>
+            <textarea
+              id='message'
+              value={form.message}
+              onChange={e => setForm({ ...form, message: e.target.value })}
+              placeholder='message'
+            />
+            <button type='submit'>Send</button>
+          </form>
+          <p>{JSON.stringify(serverMessage) ? serverMessage.msg : null}</p>
+        </motion.div>
       </motion.div>
       <div className='circle50'></div>
-        <div className='circle60'></div>
+      <div className='circle60'></div>
     </>
   )
 }
